2f4bb218ded4828c3fea2f9d15adea0675dbe35b
Adrian Lisiecki <adrianlski@gmail.com>
2019-03-31 18:14:45 +0100

    Initial commit

---
+++ BasketApi/Models/Basket.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BasketApi.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public List<Item> Items { get; set; }
+    }
+}
